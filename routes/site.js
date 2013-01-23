37e24ab16911d4753bbb143515171ec44a38cff4
Cameron Drake <cdrake757@gmail.com>
2013-01-22 16:04:04 -0800

    Added the contact page

--- routes/site.js
+++ routes/site.js
@@ -1,6 +1,24 @@
 exports.index = function(req, res){  
   res.render('index');
 };
+exports.about = function(req, res){  
+  res.render('about');
+};
+exports.classes = function(req, res){  
+  res.render('classes');
+};
+exports.educators = function(req, res){  
+  res.render('educators');
+};
+exports.culture = function(req, res){  
+  res.render('culture');
+};
+exports.saloned = function(req, res){  
+  res.render('saloned');
+};
+exports.contact = function(req, res){  
+  res.render('contact');
+};
 exports.error = function(req, res){
   res.render('404', {title: 'You Dun Fd Up' });
 };
