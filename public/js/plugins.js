fcad854066477831f264f0c798e794a064b1b059
Cameron Drake <cdrake757@gmail.com>
2013-02-04 10:50:50 -0800

    tidy

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -1,8 +1,21 @@
+$(document).ready(function(){
+	// Index Page Slideshow
+	$(function() {
+  		$("#slideshow > div:gt(0)").hide();
+  		setInterval(function() {
+  			$('#slideshow > div:first').fadeOut(1000).next().fadeIn(1000).end().appendTo('#slideshow');
+  			},  3000);
+  		});
+	
+	$('.cutTitle').addClass('animated fadeInLeft');
+	$('.signUp').addClass('animated rollIn');
+});      
+// Hover Animations
 $('nav >ul >li').hover(
 	function(){$(this).addClass('animated pulse')},
 	function(){$(this).removeClass('animated pulse')}
 );
-$('#feature1').hover(
+$('.feature-small').hover(
 	function(){$(this).addClass('animated swing')},
 	function(){$(this).removeClass('animated swing')}
 );
