9060fa77f07c3b7f00bfe6cafd09a2ad80a5f690
Cameron Drake <cdrake757@gmail.com>
2013-02-04 10:36:23 -0800

    getting those animations in as examples

---
+++ public/js/plugins.js
@@ -0,0 +1,8 @@
+$('nav >ul >li').hover(
+	function(){$(this).addClass('animated pulse')},
+	function(){$(this).removeClass('animated pulse')}
+);
+$('#feature1').hover(
+	function(){$(this).addClass('animated swing')},
+	function(){$(this).removeClass('animated swing')}
+);
