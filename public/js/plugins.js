70b30ef2493f55881e3f194a69d685f62bffc648
cdrake757 <cdrake757@gmail.com>
2013-04-01 23:16:49 +0000

    fixed the style for the navigation

--- public/js/plugins.js
+++ public/js/plugins.js
@@ -1,14 +1,3 @@
-/*$(window).scroll(function(e){ 
-  $el = $('nav'); 
-  if ($(this).scrollTop() > 179 && $el.css('position') != 'fixed' && $('header').length){ 
-    $('.navContainer').css({'position': 'fixed', 'top': '0px', 'background': 'rgba(0,0,0,1)'});
-    $('#logoSmall').css({'opacity': '1'});
-  } else if ($(this).scrollTop() < 180 && $el.css('position') != 'absolute' && $('header').length){ 
-    $('.navContainer').css({'position': 'absolute', 'top': '180px', 'background': 'rgba(0,0,0,0)'});
-    $('#logoSmall').css({'opacity': '0'});
-  } 
-});*/
-
 $(document).ready(function(){
 	// Index Page Slideshow
 	$(function() {
@@ -20,10 +9,6 @@ $(document).ready(function(){
 	
 	$('.cutTitle').addClass('animated fadeInLeft');
 	$('#logo').addClass('animated fadeInLeft');
-	if($('header').length) {
-		$('.navContainer').css({'position': 'absolute', 'top': '180px', 'background': 'rgba(0,0,0,0)'});
-	    $('#logoSmall').css({'opacity': '0'});
-	}
 });      
 // Hover Animations
 $('nav ul li').hover(
@@ -31,7 +16,7 @@ $('nav ul li').hover(
 	function(){$(this).removeClass('animated pulse')}
 );
 $('.feature-small').hover(
-	function(){$(this).addClass('animated tada')},
+	function(){$(this).addClass('animated pulse')},
 	function(){$(this).removeClass('animated pulse')}
 );
 $('.footerContainer footer ul li').hover(
